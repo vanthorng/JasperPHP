06f3b44a0b869468883688d3a8adbc17606fd8b1
vanthorng <T.Vanthorng@cas.com.kh>
2025-03-21 14:41:53 +0700

    Test
    Here arre the samplpe test commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'quilhasoft/jasperphp',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3af999fe4d5b35cdf764a3af279edd125d4ce82f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'quilhasoft/jasperphp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3af999fe4d5b35cdf764a3af279edd125d4ce82f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.8.2',
+            'version' => '6.8.2.0',
+            'reference' => 'f7a781073e1645062f163e058139e2f89355d420',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
